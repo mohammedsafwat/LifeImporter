b0c33a741414bbeb73f236a4b01abbc62cbee78f
Mohammed Safwat <msafwat@souq.com>
2016-09-06 22:10:43 +0400

    feat: Add a new category for Category model class to add the NSManaged properties.

---
+++ LifeImporter/Models/Category+CoreDataProperties.swift
@@ -0,0 +1,15 @@
+//
+//  Category+CoreDataProperties.swift
+//  LifeImporter
+//
+//  Created by Mohammed Safwat on 9/5/16.
+//  Copyright © 2016 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+extension Category {
+    @NSManaged var category : String?
+    @NSManaged var food : Food?
+}
