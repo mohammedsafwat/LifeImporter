86b1410d6109aa08eff09bd5a9a455bb57b6a443
Mohammed Safwat <msafwat@souq.com>
2016-09-06 22:08:16 +0400

    feat: Add new singleton class to store all of the shared data across the application.

---
+++ LifeImporter/Models/AppSharedSettings/AppSharedData.swift
@@ -0,0 +1,29 @@
+//
+//  AppSharedSettings.swift
+//  LifeImporter
+//
+//  Created by Mohammed Safwat on 9/5/16.
+//  Copyright © 2016 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+
+class AppSharedData: NSObject {
+    static let sharedInstance = AppSharedData()
+    
+    enum FileModelType {
+        case Food, Exercise, Category
+    }
+    
+    enum HomeScreenOptionType {
+        case Food, Exercise, Category
+    }
+    
+    let homeScreenOptions = [HomeScreenOptionType.Food, HomeScreenOptionType.Exercise, HomeScreenOptionType.Category]
+    
+    let homeScreenOptionsStringNames : [HomeScreenOptionType : String] = [.Food : "Food", .Exercise : "Exercise", .Category : "Category"]
+    
+    let homeScreenOptionsEntityNames : [HomeScreenOptionType : String] = [.Food : "Food", .Exercise : "Exercise", .Category : "Category"]
+
+    let heightForTableViewRow : CGFloat = 64.0
+}
