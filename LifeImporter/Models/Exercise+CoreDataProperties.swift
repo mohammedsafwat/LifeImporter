c2071c6b589866e7d7f16b411cfa15f0880bce66
Mohammed Safwat <msafwat@souq.com>
2016-09-06 22:09:49 +0400

    feat: Add a new category for Exercise model class to add the NSManaged properties.

---
+++ LifeImporter/Models/Exercise+CoreDataProperties.swift
@@ -0,0 +1,14 @@
+//
+//  Exercise+CoreDataProperties.swift
+//  LifeImporter
+//
+//  Created by Mohammed Safwat on 9/5/16.
+//  Copyright © 2016 Mohammed Safwat. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+extension Exercise {
+    @NSManaged var title : String?
+}
